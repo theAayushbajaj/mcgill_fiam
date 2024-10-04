4c2318ede9dd3b668d7ce87101b850e7b2bc752c
Uday Karan Kapur <udaykarankapur@gmail.com>
2024-10-03 20:25:45 -0400

    clean discovery.py

--- 0X-Causal_discovery/discovery.py
+++ 0X-Causal_discovery/discovery.py
@@ -1,8 +1,14 @@
+"""
+This script runs causal discovery on the given financial data to find correlated features.
+"""
+
 import avici
 import pandas as pd
 import networkx as nx
 
 model = avici.load_pretrained(download="scm-v0")
+
+TGT_VAR = 'target'
 
 # load top_100_features.json
 top_100_features = pd.read_json('../0X-Causal_discovery/top_100_features.json')
@@ -20,9 +26,9 @@ df = x.copy()
 x = x.to_numpy()
 g_prob = model.predict(x)
 
-threshold = 0.02
+THRESHOLD = 0.02
 
-adj_matrix = (g_prob > threshold).astype(int)
+adj_matrix = (g_prob > THRESHOLD).astype(int)
 
 G = nx.DiGraph(adj_matrix)
 
@@ -31,15 +37,13 @@ variable_names = x.columns.tolist()
 mapping = {i: var_name for i, var_name in enumerate(variable_names)}
 G = nx.relabel_nodes(G, mapping)
 
-target_variable = 'target'
-features = [var for var in variable_names if var != target_variable]
+features = [var for var in variable_names if var != TGT_VAR]
 
 
 # Assuming G is your causal DAG and features is your list of feature nodes
-target_variable = 'target'
 
 for feature in features:
-    if nx.has_path(G, source=feature, target=target_variable):
-        print(f"There is a path from '{feature}' to '{target_variable}'.")
+    if nx.has_path(G, source=feature, target=TGT_VAR):
+        print(f"There is a path from '{feature}' to '{TGT_VAR}'.")
     else:
-        print(f"No path exists from '{feature}' to '{target_variable}'.")
+        print(f"No path exists from '{feature}' to '{TGT_VAR}'.")
